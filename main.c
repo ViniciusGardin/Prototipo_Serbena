26238a05156cf25d9a230332b8c1953bef42a778
ViniciusGardin <3010vinicius@gmail.com>
2021-07-20 15:40:53 -0300

    cont Marlio main

--- main.c
+++ main.c
@@ -2,11 +2,10 @@
 #include "stm32f10x_conf.h"
 #include "utils.h"
 
-uint16_t data = 0;
-int DMA_flag = 0;
-
-uint8_t i = 0;
-uint8_t count = 0;
+uint16_t data = 0;	//Valor de conversão do ADC
+int DMA_flag = 0;	//Flag que a interrupção do DMA ativa
+uint8_t i = 0;		//Estagio da onda senoidal estamos analisando
+uint8_t count = 0;	//Quantas vezes foi feita as mesmas medidas
 
 long Real = 0;
 long Imag = 0;
@@ -28,6 +27,7 @@ const uint16_t HFavg = 128;
 const uint16_t LFavg = HFavg >> 2;
 const uint16_t avg = LFavg;
 const uint16_t LFlim = 20;
+const uint16_t HFlim = 100000;
 
 //Funções que não sei se vou usar
 //void DMA_SetCurrDataCounter(DMA_Channel_TypeDef* DMAy_Channelx, uint16_t DataNumber); 
@@ -51,47 +51,58 @@ int main(void) {
 	//sleep_AD9833();
 
 	ADC_Cmd(ADC1, ENABLE);
-  	ADC_SoftwareStartConvCmd(ADC1, ENABLE);
+ 	ADC_SoftwareStartConvCmd(ADC1, ENABLE);
 	while(1) {
 		if(DMA_flag) {
 			ADC_Cmd(ADC1, DISABLE);
 			switch(i) {
 				case 1://Sample at falling slope of sinus
-						Imag = (long) (-1)*data;
-						init_EXT(RISE);
-						ADC_Cmd(ADC1, ENABLE);
-  						ADC_SoftwareStartConvCmd(ADC1, ENABLE);
-						break;
+					Imag = (long) (-1)*data;
+					init_EXT(RISE);
+					ADC_Cmd(ADC1, ENABLE);
+  					ADC_SoftwareStartConvCmd(ADC1, ENABLE);
+					i++;
+					break;
 				case 2://sample at negative peak of sinus
-						Real = (long) (-1)*data;
-						init_EXT(FALL);
-						ADC_Cmd(ADC1, ENABLE);
-  						ADC_SoftwareStartConvCmd(ADC1, ENABLE);
-						break;
+					Real = (long) (-1)*data;
+					init_EXT(FALL);
+					ADC_Cmd(ADC1, ENABLE);
+  					ADC_SoftwareStartConvCmd(ADC1, ENABLE);
+					i++;
+					break;
 				case 3://Sample at falling slope of sinus
-						Imag += (long) data;
-						init_EXT(RISE);
-						ADC_Cmd(ADC1, ENABLE);
-  						ADC_SoftwareStartConvCmd(ADC1, ENABLE);
-						break;
+					Imag += (long) data;
+					init_EXT(RISE);
+					ADC_Cmd(ADC1, ENABLE);
+  					ADC_SoftwareStartConvCmd(ADC1, ENABLE);
+					i++;
+					break;
 				case 4://sample at positive peak of sinus
-						Real += (long) data;
-						init_EXT(FALL);
-						ADC_Cmd(ADC1, ENABLE);
-  						ADC_SoftwareStartConvCmd(ADC1, ENABLE);
-						break;
+					Real += (long) data;
+					init_EXT(FALL);
+					ADC_Cmd(ADC1, ENABLE);
+  					ADC_SoftwareStartConvCmd(ADC1, ENABLE);
+					i++;
+					break;
 				case 5://End of Sinus
-						i = 0;
-						freq *= fincr;
-						if(freq < LFlim)
-								avg = LFavg;
-						else
-								avg = HFavg;
-						setFrequency(freq);
-						reset_AD9833();
-						ADC_Cmd(ADC1, ENABLE);
-  						ADC_SoftwareStartConvCmd(ADC1, ENABLE);
-			}
+					i = 0;
+					freq *= fincr;
+					if(freq < LFlim)
+						avg = LFavg;
+					else
+						avg = HFavg;
+					if(freq > HFlim){
+						sleep_AD9833();
+						//Aqui os valores de Real e Imag prontos para cada 
+						//frequencia
+						while(1);
+					}
+							
+					setFrequency(freq);
+					reset_AD9833();
+					ADC_Cmd(ADC1, ENABLE);
+  					ADC_SoftwareStartConvCmd(ADC1, ENABLE);
+		}
 
 		}
 	}
