798d8e020752e44b739572ddcf45fd9faeac3170
ViniciusGardin <3010vinicius@gmail.com>
2021-08-29 17:25:31 -0300

    v0.3

--- Programa_SerbenaINO/Programa_SerbenaINO.ino
+++ Programa_SerbenaINO/Programa_SerbenaINO.ino
@@ -1,11 +1,7 @@
  /*
- * TODO: EXTI!!!! 
- *
- * TODO: RCC
- *
- * TODO: Fazer verificação no sysclock
- *
  * TODO: Fazer a calibração
+ *
+ * TODO: Calculuar os sampletimes dnv
  *
  * TODO: Valor de res
  *
@@ -46,42 +42,22 @@
 #define SLEEP             0x180 
 #define REG0              0x4000//Registrador 0
 
-//Endereço para configurar o ADC do STM
-#define ADC1_DR_Address     ((uint32_t)0x4001244C)  
-
 #define NPTS 101  //numero de pontos = 101
 
-enum {
-  RISE = 0,
-  FALL
-};
-
-void reset_AD9833();
-void init_AD9833(float frequency);
-void setFrequency(float frequency);
-void sleep_AD9833();
-void writeRegisterA( uint16_t command );
-void writeRegisterB( uint16_t command );
-void exti_Interrupt() {
-void dma_Interrupt();
-
-uint8_t adc_pin = PA0;//Para usa o endenreço de PA0
-uint8_t adc_enable = 0;//Para usa o endenreço de PA0
-
-uint16_t data = 0;    //Valor de conversão do ADC
-uint8_t DMA_flag = 0; //Flag que a interrupção do DMA ativa
-uint8_t count = 0;    //Quantas vezes foi feita as mesmas medidas
+uint8_t adc_pin = PA0;	//Para usa o endenreço de PA0
+uint8_t adc_enable = 0;	//Para usa o endenreço de PA0
+uint8_t DMA_flag = 0; 	//Flag que a interrupção do DMA ativa
+uint8_t count = 0;    	//Quantas vezes foi feita as mesmas medidas
 uint8_t wavePoint = 1;  //Ponto da onda senoidal estamos analisando
-uint8_t nptsA = 0;    //numero do ponto atual
+uint8_t nptsA = 0;    	//numero do ponto atual
+uint16_t data = 0;    	//Valor de conversão do ADC
 
-
-long RealVar = 0; //Variavel do valor real
-long ImagVar = 0; //Variavel do valor imaginario
-long Real[NPTS];  //Todos os valores reais da tensão
-long Imag[NPTS];  //Todos os valores imaginarios da tensão
+long RealVar = 0; 	//Variavel do valor real
+long ImagVar = 0; 	//Variavel do valor imaginario
+long Real[NPTS];  	//Todos os valores reais da tensão
+long Imag[NPTS];  	//Todos os valores imaginarios da tensão
 long Zmodulo[NPTS]; //Valores da impedancia no final
 double freqA[NPTS]; //Valores da frequencia para cada ponto
-
 
 //Valores da calibração que são fixos do sistema. São obtidos previamente
 //em uma medição com curto-circuito
@@ -107,15 +83,34 @@ const uint16_t LFlim = 20;
 const uint32_t HFlim = 100000;
 //* O operador ">>" é igual a dividir por 2^(numero usado no operador)
 
+/*******************************************************************/
+/*                           Funções                               */
+/*******************************************************************/
+
+enum {
+	RISE = 0,
+	FALL
+};
+
+void reset_AD9833();
+void init_AD9833(float frequency);
+void setFrequency(float frequency);
+void sleep_AD9833();
+void writeRegisterA(uint16_t command);
+void writeRegisterB(uint16_t command);
+
+void sampleTime(double freq);
+void init_EXT(int externalTrigger);
+void exti_Interrupt();
+void dma_Interrupt();
 
 /*******************************************************************/
 /*                          void setup                             */
 /*******************************************************************/
-
 STM32ADC myADC(ADC1);
 
 void setup() {
-	while (!Serial); delay(10);
+	Serial.begin(115200); 
 	Serial.print("Setup...");
 
 	/* Configure SPI1 ------------------------------------------------------------------*/
@@ -127,15 +122,14 @@ void setup() {
 	pinMode(SPI1_FSYNCB_PIN, OUTPUT);
 	
 	/* Configure ADC1 ------------------------------------------------------------------*/
+	rcc_set_prescaler(RCC_PRESCALER_AHB, RCC_AHB_SYSCLK_DIV_1);//24MHz
+	rcc_set_prescaler(RCC_PRESCALER_APB2, RCC_APB2_HCLK_DIV_1);//24Mhz
+	adc_set_prescaler(ADC_PRE_PCLK2_DIV_2);//12MHz (Maximo do ADC)
 	myADC.setPins(&adc_pin, 1);
 	sampleTime(freq);
 	myADC.setDMA(&data, 1,(DMA_TRNS_CMPLT | DMA_CIRC_MODE), dma_Interrupt);
 	myADC.calibrate();
 	init_EXT(FALL);
-
-	
-	
-	Serial.begin(115200); 
 	
 	Serial.println("done.");
 }
@@ -145,54 +139,50 @@ void setup() {
 /*******************************************************************/
  
 void loop() {
-  //É iniciado os valores de cada vetor
-  freqA[nptsA] = freq;
-  for(int i = 0; i < NPTS; i++) {
-      Real[i] = 0;
-      Imag[i] = 0;
-  }
+	//É iniciado os valores de cada vetor
+	freqA[nptsA] = freq;
+	for(int i = 0; i < NPTS; i++) {
+		Real[i] = 0;
+		Imag[i] = 0;
+	}
 	adc_enable = 1;
-   while(1) {
-    while(count < avg){
-      while(1) {
-        if(DMA_flag) {
-          //ADC_Cmd(ADC1, DISABLE); Se necessario
-          switch(wavePoint) {
-            case 1://Sample at falling slope of sinus
-              ImagVar = -((data)>>1);
-              init_EXT(RISE);
-              wavePoint++;
-              //ADC_Cmd(ADC1, ENABLE);
-              break;
-            case 2://Sample at negative peak of sinus
-              RealVar = -((data)>>1);
-              init_EXT(FALL);
-              wavePoint++;
-              //ADC_Cmd(ADC1, ENABLE);
-              break;
-            case 3://Sample at falling slope of sinus
-              ImagVar += ((data)>>1);
-              init_EXT(RISE);
-              wavePoint++;
-              //ADC_Cmd(ADC1, ENABLE);
-              break;
-            case 4://Sample at positive peak of sinus
-              RealVar += ((data)>>1);
-              wavePoint++;
-              //ADC_Cmd(ADC1, ENABLE);
-              break;
-            case 5://End of Sinus
-              Real[nptsA] += RealVar/NPTS;
-              Imag[nptsA] += ImagVar/NPTS;
-              wavePoint = 1;
-              break;
-          }//End os switch
-          break;//Para sair do segundo while(1)
-        }//End of if(DMA_flag)
-      }//End of second while(1)
-      DMA_flag = 0;
-      count++;
-    }//End of while(count < avg)
+	while(1) {
+		while(count < avg){
+			while(1) {
+				if(DMA_flag) {
+					switch(wavePoint) {
+						case 1://Sample at falling slope of sinus
+							ImagVar = -((data)>>1);
+							init_EXT(RISE);
+							wavePoint++;
+							break;
+							case 2://Sample at negative peak of sinus
+							RealVar = -((data)>>1);
+							init_EXT(FALL);
+							wavePoint++;
+							break;
+						case 3://Sample at falling slope of sinus
+							ImagVar += ((data)>>1);
+							init_EXT(RISE);
+							wavePoint++;
+							break;
+						case 4://Sample at positive peak of sinus
+							RealVar += ((data)>>1);
+							wavePoint++;
+							break;
+						case 5://End of Sinus
+							Real[nptsA] += RealVar/NPTS;
+							Imag[nptsA] += ImagVar/NPTS;
+							break;
+					}//End os switch
+					DMA_flag = 0;
+					if(wavePoint == 5)
+						break;//Para sair do segundo while(1)
+				}//End of if(DMA_flag)
+			}//End of second while(1)
+		wavePoint = 1;
+		count++;
+		}//End of while(count < avg)
 	adc_enable = 0;
 	Serial.print("Frequencia: ");
 	Serial.print(freq);
@@ -200,11 +190,11 @@ void loop() {
 	Serial.print(Real[nptsA]);
 	Serial.print("\tTensão Imaginaria: ");
 	Serial.println(Imag[nptsA]);
-    count = 0;
-    nptsA++;
+	count = 0;
+	nptsA++;
 	if(nptsA >= NPTS){//Aqui acabou 
-    	sleep_AD9833();
-      	//Calculo da impedancia para cada frequencia
+		sleep_AD9833();
+		//Calculo da impedancia para cada frequencia
 		for(int i = 0; i<NPTS; i++) {
 			long var = res/(pow((Real[i]+RealOpen[i]),2)+pow((Imag[i] + ImagOpen[i]),2));
 			long Zimag = Imag[i]*(Real[i]+RealOpen[i])-Real[i]*(Imag[i]+ImagOpen[i]);
@@ -217,21 +207,21 @@ void loop() {
 			Serial.print(freqA[i]);
 			Serial.print("\tImpedancia: ");
 			Serial.println(Zmodulo[i]);
-      	}
-      	while(1){__nop();}
-    }
-    freq *= fincr;
-    freqA[nptsA] = freq;
-    sampleTime(freq);
-    setFrequency(freq);
-    if(freq < LFlim)
-      avg = LFavg;
-    else
-      avg = HFavg;
-    init_EXT(FALL);
+		}
+		while(1){__nop();}
+	}
+	freq *= fincr;
+	freqA[nptsA] = freq;
+	sampleTime(freq);
+	setFrequency(freq);
+	if(freq < LFlim)
+		avg = LFavg;
+	else
+		avg = HFavg;
+	init_EXT(FALL);
+	reset_AD9833();
 	adc_enable = 1;
-    reset_AD9833();
-  }//End of while(1)
+	}//End of while(1)
 }//End of main
 
 /*******************************************************************/
@@ -245,25 +235,25 @@ void loop() {
  * frequencia e fase.
  */
 void reset_AD9833() {
-  digitalWrite(SPI1_FSYNCA_PIN, LOW);
-  digitalWrite(SPI1_FSYNCB_PIN, LOW);
+	digitalWrite(SPI1_FSYNCA_PIN, LOW);
+  	digitalWrite(SPI1_FSYNCB_PIN, LOW);
 
-  SPI.transfer(RESET_CMD);
-  delay(15);
-  SPI.transfer(NORMAL_CMD);
+  	SPI.transfer(RESET_CMD);
+  	delay(15);
+  	SPI.transfer(NORMAL_CMD);
 
-  digitalWrite(SPI1_FSYNCA_PIN, HIGH);
-  digitalWrite(SPI1_FSYNCB_PIN, HIGH);
+  	digitalWrite(SPI1_FSYNCA_PIN, HIGH);
+  	digitalWrite(SPI1_FSYNCB_PIN, HIGH);
 }
 
 /*
  * Inicia o AD9833. Frequency in Hz
  */
 void init_AD9833(float frequency) {
-  reset_AD9833();
-  writeRegisterA(SINE_WAVE);
-  writeRegisterB(SQUARE_WAVE);
-  setFrequency(frequency);
+	reset_AD9833();
+  	writeRegisterA(SINE_WAVE);
+  	writeRegisterB(SQUARE_WAVE);
+  	setFrequency(frequency);
 }
 
 /*
@@ -312,41 +302,20 @@ void sleep_AD9833() {
 }
 
 void writeRegisterA( uint16_t command ) {
-  digitalWrite(SPI1_FSYNCA_PIN, LOW);
-  SPI.transfer(command);
-  digitalWrite(SPI1_FSYNCA_PIN, HIGH);
+	digitalWrite(SPI1_FSYNCA_PIN, LOW);
+  	SPI.transfer(command);
+  	digitalWrite(SPI1_FSYNCA_PIN, HIGH);
 }
 
 void writeRegisterB( uint16_t command ) {
- digitalWrite(SPI1_FSYNCB_PIN, LOW);
-  SPI.transfer(command);
-  digitalWrite(SPI1_FSYNCB_PIN, HIGH);
+	digitalWrite(SPI1_FSYNCB_PIN, LOW);
+  	SPI.transfer(command);
+  	digitalWrite(SPI1_FSYNCB_PIN, HIGH);
 }
 
 /*******************************************************************/
 /*               Funções de configuração do STM                    */
 /*******************************************************************/
-void init_Clock() {
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA  |
-              RCC_APB2Periph_GPIOB |
-              RCC_APB2Periph_GPIOC |
-              RCC_APB2Periph_ADC1  |
-              RCC_APB2Periph_SPI1  | 
-            RCC_APB2Periph_AFIO,  ENABLE);
-
-  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
-  //Considerando 56MHz = HCLK = PCLK2
-  RCC_ADCCLKConfig(RCC_PCLK2_Div4); 
-
-  //#if defined (STM32F10X_LD_VL) || defined (STM32F10X_MD_VL) || defined (STM32F10X_HD_VL)
-  //  /* ADCCLK = PCLK2/2 */
-  //  RCC_ADCCLKConfig(RCC_PCLK2_Div2); 
-  //#else
-  //  /* ADCCLK = PCLK2/4 */
-  //  RCC_ADCCLKConfig(RCC_PCLK2_Div4); 
-  //#endif
-}
-
 /*
  * Sampletime altera o numero de ciclos utilizados para fazer a amostra da
  *  tensão, quanto maior, maior o tempo e a qualidade da conversão. Quanto
@@ -354,33 +323,30 @@ void init_Clock() {
  */
 
 void sampleTime(double freq) {
-
-  if(freq < 13157){
-    setSampleRate(ADC_SMPR_239_5);
-
-  }
-  else if(freq < 35714){
-    setSampleRate(ADC_SMPR_71_5);
-
-  }
-  else if(freq < 42682){
-    setSampleRate(ADC_SMPR_55_5);
-  }
-  else if(freq < 51470){
-    setSampleRate(ADC_SMPR_41_5);
-  }
-  else if(freq < 63636){
-    setSampleRate(ADC_SMPR_28_5);
-  }
-  else if(freq < 87500){
-    setSampleRate(ADC_SMPR_13_5);
-  }
-  else if(freq < 102940){
-    setSampleRate(ADC_SMPR_7_5);
-  }
-  else{
-    setSampleRate(ADC_SMPR_1_5);
-  }
+	if(freq < 13157){
+  		setSampleRate(ADC_SMPR_239_5);
+  	}
+  	else if(freq < 35714){
+  		setSampleRate(ADC_SMPR_71_5);
+  	}
+  	else if(freq < 42682){
+  		setSampleRate(ADC_SMPR_55_5);
+  	}
+  	else if(freq < 51470){
+  		setSampleRate(ADC_SMPR_41_5);
+  	}
+  	else if(freq < 63636){
+  		setSampleRate(ADC_SMPR_28_5);
+  	}
+  	else if(freq < 87500){
+  		setSampleRate(ADC_SMPR_13_5);
+  	}
+  	else if(freq < 102940){
+  		setSampleRate(ADC_SMPR_7_5);
+  	}
+  	else{
+  		setSampleRate(ADC_SMPR_1_5);
+  	}
 }
 /*
  * External event on PA00 RISE or FALL
@@ -399,5 +365,5 @@ void exti_Interrupt() {
 	}
 }
 void dma_Interrupt() {
-  DMA_flag = 1;
+	DMA_flag = 1;
 }
